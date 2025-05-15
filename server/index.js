c5ac29d1afffe0b82789c274dee01ced7845fbd3
satyam <sars@gmail.com>
2025-05-15 19:10:58 +0530

    mongoose updated

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ require('dotenv').config();
 require('./config/passport');
 
 // Connect DB
-mongoose.connect('mongodb://localhost:27017/user', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/user');
 
 // CORS should come BEFORE sessions
 app.use(cors({
